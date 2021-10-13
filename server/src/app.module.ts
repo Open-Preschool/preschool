e1405a0c69460b05c327301569d7a0dfc9327c6e
Tyler Sustare <tylersustare@gmail.com>
2021-10-12 22:56:44 -0700

    Add GQL to server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { join } from 'path';
+import { ClassroomModule } from './classroom/classroom.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    ClassroomModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
